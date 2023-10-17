70db1d4ac7db897e4735147d8fb722b57f66a2d8
Alan Nato <50413623+iamnotnato@users.noreply.github.com>
2023-10-17 08:50:27 +0300

    fix : Where can I learn Python?

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-'''Task 11's module.
+'''a Python function that returns the list of school having a specific topic
 '''
 
-
 def schools_by_topic(mongo_collection, topic):
-    '''Returns the list of school having a specific topic.
+    '''a Python function that returns the list of school having a specific topic
     '''
     topic_filter = {
         'topics': {
